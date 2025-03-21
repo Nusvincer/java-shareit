10822ef88fcb08a763bd1c606a9ec06d2d727d8e
yandere <nusvincer@gmail.com>
2025-03-21 17:07:58 +0400

    review fix

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -2,7 +2,9 @@ package ru.practicum.shareit.booking;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.ItemMapper;
+import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserMapper;
 
 public class BookingMapper {
@@ -17,13 +19,13 @@ public class BookingMapper {
         );
     }
 
-    public static Booking toEntity(BookingDto dto) {
+    public static Booking toEntity(BookingDto dto, Item item, User booker) {
         return new Booking(
                 dto.getId(),
                 dto.getStart(),
                 dto.getEnd(),
-                null,
-                null,
+                item,
+                booker,
                 dto.getStatus()
         );
     }
--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -4,6 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.ItemRepository;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.UserRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,10 +17,17 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class BookingServiceImpl implements BookingService {
     private final BookingRepository bookingRepository;
+    private final ItemRepository itemRepository;
+    private final UserRepository userRepository;
 
     @Override
     public BookingDto createBooking(BookingDto bookingDto) {
-        Booking booking = BookingMapper.toEntity(bookingDto);
+        Item item = itemRepository.findById(bookingDto.getItem().getId())
+                .orElseThrow(() -> new NotFoundException("Item not found"));
+        User booker = userRepository.findById(bookingDto.getBooker().getId())
+                .orElseThrow(() -> new NotFoundException("User not found"));
+
+        Booking booking = BookingMapper.toEntity(bookingDto, item, booker);
         return BookingMapper.toDto(bookingRepository.save(booking));
     }
 
@@ -23,7 +35,7 @@ public class BookingServiceImpl implements BookingService {
     public BookingDto getBookingById(Long id) {
         return bookingRepository.findById(id)
                 .map(BookingMapper::toDto)
-                .orElseThrow(() -> new RuntimeException("Booking not found"));
+                .orElseThrow(() -> new NotFoundException("Booking not found"));
     }
 
     @Override
